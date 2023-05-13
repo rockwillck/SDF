0739b8b08f486da707b8afe4517b4c910e9d6156
rockwill <will.choi.kim@gmail.com>
2023-05-13 18:21:15 -0500

    Updated demo

--- script.js
+++ script.js
@@ -1,24 +1,59 @@
 const canvas = document.getElementById("canvas")
 const ctx = canvas.getContext("2d")
-canvas.width = 1024
-canvas.height = 1024
+canvas.width = window.innerWidth
+canvas.height = window.innerHeight
 
-var goopiness = 100
-function goopChange(newGoopiness) {
-    goopiness = newGoopiness
-    document.getElementById("goopLabel").innerText = `Goopiness (${goopiness})`
+function resize() {
+    scaleFactor = Math.sqrt(window.innerWidth**2 + window.innerHeight**2)/Math.sqrt(canvas.width**2 + canvas.height**2)
+    oldCenter = {x:canvas.width/2, y:canvas.height/2}
+    canvas.width = window.innerWidth
+    canvas.height = window.innerHeight
+    metagons.forEach((mtgn) => {
+        mtgn.vertices.forEach((vertex) => {
+            vertex.x = canvas.width/2 + (vertex.x - oldCenter.x)*scaleFactor
+            vertex.y = canvas.height/2 + (vertex.y - oldCenter.y)*scaleFactor
+        })
+        mtgn.calculateCenter()
+    })
+}
+
+function goopChange(slider, newGoopiness, mtgnID) {
+    for (mtgn of metagons) {
+        if (mtgn.id == mtgnID) {
+            mtgn.gravity = newGoopiness
+            document.getElementById(`goopLabel${mtgn.id}`).innerText = `[${mtgn.name}] Goopiness (${newGoopiness})`
+            break
+        }
+    }
+}
+
+function makeMtgnActive(container) {
+    console.log(container)
+    container.style.backgroundColor = "gray"
+}
+
+var mouseActive = true
+function sliderActive(boolean) {
+    mouseActive = !boolean
 }
 
 function normalize(force) {
     return {x:force.x/Math.sqrt(force.x**2 + force.y**2), y:force.y/Math.sqrt(force.x**2 + force.y**2)}
 }
 
+function guidGenerator() {
+    return Math.random()*100
+}
+
+var metagons = []
 class Polygon {
-    constructor(vertices, gravity=1) {
+    constructor(vertices, gravity=100, name=`Metagon ${metagons.length + 1}`) {
         this.vertices = vertices
         this.gravity = gravity
         this.bufferVertices = vertices
         this.calculateCenter()
+        this.name = name
+        this.id = guidGenerator()
     }
 
     calculateCenter() {
@@ -31,20 +66,22 @@ class Polygon {
         this.center.y /= this.vertices.length
     }
 
-    update(objects) {
+    update(metagons) {
         this.bufferVertices = []
         this.vertices.forEach((vertex) => {
             let pull = {x:0, y:0}
-            let totalGravity = this.gravity
-            objects.forEach((obj) => {
-                totalGravity += obj.gravity
-                let pullChange = normalize({x:obj.center.x - vertex.x, y:obj.center.y - vertex.y})
-                pull.x += pullChange.x*obj.gravity
-                pull.y += pullChange.y*obj.gravity
+            let totalGravity = 1
+            metagons.forEach((mtgn) => {
+                if (mtgn != this) {
+                    totalGravity += 1
+                    let pullChange = normalize({x:mtgn.center.x - vertex.x, y:mtgn.center.y - vertex.y})
+                    pull.x += pullChange.x*mtgn.gravity
+                    pull.y += pullChange.y*mtgn.gravity
+                }
             })
             pull.x /= totalGravity
             pull.y /= totalGravity
-            this.bufferVertices.push({x:vertex.x + pull.x*goopiness, y:vertex.y + pull.y*goopiness})
+            this.bufferVertices.push({x:vertex.x + pull.x, y:vertex.y + pull.y})
         })
         this.calculateCenter()
     }
@@ -74,10 +111,10 @@ class Polygon {
 
 function generateCircle(x, y, radius) {
     let circleVertices = []
-    for (theta=0;theta<Math.PI*2;theta+=Math.PI/20) {
+    for (theta=0;theta<Math.PI*2;theta+=Math.PI/200) {
         circleVertices.push({x:x+Math.cos(theta)*radius, y:y+Math.sin(theta)*radius})
     }
-    return new Polygon(circleVertices)
+    return new Polygon(circleVertices, 300)
 }
 
 var mouse = [0, 0]
@@ -85,16 +122,30 @@ window.addEventListener(("mousemove"), (e) => {
     mouse = [e.clientX, e.clientY]
 })
 
-var objects = [generateCircle(canvas.width/2, canvas.height/2, 100), generateCircle(canvas.width/2 - 150, canvas.height/2, 100)]
+for (i=0;i<2;i++) {
+    metagons.push(generateCircle(canvas.width*Math.random(), canvas.height*Math.random(), Math.sqrt(canvas.width**2 + canvas.height**2)*0.05))
+}
+metagons.forEach((mtgn) => {
+    document.getElementById("goopSliderDiv").innerHTML += `
+    <button class="metagonAdj" id="${mtgn.id} onclick="makeMtgnActive(this)">
+    <label id="goopLabel${mtgn.id}" for="goopiness">[${mtgn.name}] Goopiness (${mtgn.gravity})</label>
+    <div class="slidecontainer">
+        <input type="range" class="goopinessSlider" name="goopiness" min="1" max="500" value="${mtgn.gravity}" oninput="goopChange(this, this.value, ${mtgn.id})" onmousedown="sliderActive(true)" onmouseup="sliderActive(false)">
+    </div>
+    </button>
+`
+})
 function animate() {
     requestAnimationFrame(animate)
     ctx.clearRect(0, 0, canvas.width, canvas.height)
 
-    objects[1].move(mouse[0] - objects[1].center.x, mouse[1] - objects[1].center.y)
+    if (mouseActive) {
+        metagons[1].move(mouse[0] - metagons[1].center.x, mouse[1] - metagons[1].center.y)
+    }
     ctx.fillStyle = "green"
-    objects.forEach((obj) => {
-        obj.update(objects)
-        obj.draw()
+    metagons.forEach((mtgn) => {
+        mtgn.update(metagons)
+        mtgn.draw()
     })
 }
 animate()
